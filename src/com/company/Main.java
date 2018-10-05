f9b561551e47f22938ec60bbac542eaeb9831f49
bmartinko <bennett.martinko@gmail.com>
2018-10-05 11:19:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student stu1 = new Student("Bennett", 4.565);
+        Student stu2 = new Student("Bryce", 4.92);
+        Student stu3 = new Student("Phil", 3.64);
+        Student stu4 = new Student("Steve", 2.894);
+
+        Vector<Student> Student = new Vector<>();
+        Student.add(stu1);
+        Student.add(stu2);
+        Student.add(stu3);
+        Student.add(stu4);
+
+        for (int i = 0; i < 3, i++);
+
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by bm846 on 10/5/18.
+ */
+public class Student {
+    public String name;
+    public double GPA;
+    public Student (String n, double g){
+        name = n;
+        GPA = g;
+    }
+}
